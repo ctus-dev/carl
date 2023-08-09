15dc996e2e6a860b259f9ad9bbd9611e5d8c40f4
navigator00 <mhenning@ctus.com>
2023-08-09 09:19:55 -0500

    test

--- layouts/-Dev/Record Comments/main.js
+++ layouts/-Dev/Record Comments/main.js
@@ -27,6 +27,6 @@ const logItems = [
       </div>
     `;
   });
-  
-  const logContainer = document.getElementById("log-container");
-  logContainer.innerHTML = commentHtml.join("");
+  console.log(commentHtml);
+  //const logContainer = document.getElementById("log-container");
+  //logContainer.innerHTML = commentHtml.join("");
